c6cd26294c1b2f76a381a69dda049ef283ffecdf
cleva <cleva@BETHARIAN>
2026-04-21 08:36:56 -0400

    Added Endpoints

--- ISYS366Assignment8/Program.cs
+++ ISYS366Assignment8/Program.cs
@@ -1,12 +1,16 @@
+using ISYS366Assignment3.Data;
+using ISYS366Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<MovieRepoEf>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,7 +18,47 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/api/movies", async ([FromServices] MovieRepoEf repo) =>
+{
+    return Results.Ok(await repo.GetAllAsync());
+});
 
+app.MapGet("/api/movies/{id:int}", async ([FromServices] MovieRepoEf repo, int id) =>
+{
+    var movie = await repo.GetByIdAsync(id);
+    return movie is not null ? Results.Ok(movie) : Results.NotFound();
+});
+
+app.MapPost("/api/movies", async ([FromServices] MovieRepoEf repo, [FromBody] Movie movie) =>
+{
+    await repo.AddAsync(movie);
+    return Results.Created($"/api/movies/{movie.Id}", movie);
+});
+
+app.MapPut("/api/movies/{id:int}", async ([FromServices] MovieRepoEf repo, int id, [FromBody] Movie updatedMovie) =>
+{
+    var movie = await repo.GetByIdAsync(id);
+    if (movie is null) return Results.NotFound();
+
+    movie.Title = updatedMovie.Title;
+    movie.ReleaseDate = updatedMovie.ReleaseDate;
+    movie.Genre = updatedMovie.Genre;
+    movie.Price = updatedMovie.Price;
+    movie.Rank = updatedMovie.Rank;
+    movie.ImageUri = updatedMovie.ImageUri;
+
+    await repo.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/api/movies/{id:int}", async ([FromServices] MovieRepoEf repo, int id) =>
+{
+    var movie = await repo.GetByIdAsync(id);
+    if (movie is null) return Results.NotFound();
+
+    await repo.RemoveAsync(movie);
+    return Results.NoContent();
+});
 
 app.Run();
 
